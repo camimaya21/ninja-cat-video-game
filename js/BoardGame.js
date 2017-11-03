50c19a8ca4d06628a71d8c024ca7bb987ec932e5
Camila Maya <camilamaya21@gmail.com>
2017-11-03 12:59:55 +0100

    MVP Ninja Cat Training V.02 Button added

--- js/BoardGame.js
+++ js/BoardGame.js
@@ -79,38 +79,20 @@ GameArea.prototype.init = function (){
   ctx.fillText("NinjaCat", 447.4, 64.6);
   ctx.fillText("Training", 457.0, 133.3);
 
-  // flood effect
-  ctx.globalAlpha = alpha * 1.00;
-  ctx.beginPath();
-  ctx.moveTo(495.1, 170.9);
-  ctx.lineTo(629.1, 170.9);
-  ctx.bezierCurveTo(635.6, 170.9, 740.9, 176.2, 840.9, 182.6);
-  ctx.lineTo(540.9, 254.6);
-  ctx.bezierCurveTo(640.9, 211.1, 735.6, 216.4, 829.1, 216.4);
-  ctx.lineTo(395.1, 256.4);
-  ctx.bezierCurveTo(588.7, 216.4, 583.4, 211.1, 583.4, 204.6);
-  ctx.lineTo(483.4, 182.6);
-  ctx.bezierCurveTo(583.4, 176.2, 588.7, 170.9, 595.1, 170.9);
-  ctx.closePath();
-  ctx.fillStyle = "rgb(116, 10, 10)";
-  ctx.fill();
+  ctx.fillStyle = "rgb(142, 24, 24)";
+  ctx.fillRect(475, 170, 280, 50);
 
   ctx.font = "24.0px 'Impact'";
   ctx.fillStyle = "#FFF";
-  ctx.fillText("Press ENTER to Start", 550, 200.4);
+  ctx.fillText("Press ENTER to Start", 515, 205);
 
-  ///How to Play
   ctx.fillStyle = "#222222";
   ctx.font = "24.0px 'Impact'";
   ctx.fillText("How to Play:", 300, 266.6);
 
-
   ctx.font = "14.0px 'Courier New'";
   ctx.fillText("NinjaCat wants to be one of the greatest Ninja on earth. Help", 300, 299.3);
-
   ctx.fillText("him to survive the Master Training to become an IronNinja", 300, 316.1);
-
-  ctx.font = "14.0px 'Courier New'";
   ctx.fillText("Use your keyboard to move NinjaCat, to keep him safe.  ", 300, 349.7);
   ctx.drawImage(this.imageIntro, 0, 0);
 };
@@ -122,6 +104,7 @@ GameArea.prototype.finish = function (){
   ctx.textAlign = "center";
   ctx.fillText("Your score:", 570, 140);
   ctx.fillText(Math.floor(this.points) + " points.", 570, 180);
+
   ctx.font = "28px 'Shojumaru', cursive";
   ctx.fillStyle = "#222222";
   ctx.fillText('"You need to do more KATAS"', canvas.width / 2 + 145, canvas.height / 2 - 220);
@@ -129,15 +112,13 @@ GameArea.prototype.finish = function (){
   ctx.font = "20px Arial";
   ctx.fillStyle = "rgb(116, 10, 10)";
 
-  ctx.fillStyle = "#rgb(116, 10, 10)";
-  ctx.fillRect(420, 250, 300, 50);
   ctx.fillStyle = "rgb(142, 24, 24)";
+  ctx.fillRect(420, 250, 300, 50);
 
   ctx.font = "24.0px 'Impact'";
   ctx.fillStyle = "#fff";
   ctx.fillText("Press ENTER to Try again", canvas.width / 2 + 140, 260);
 
-  ///How to Play
   ctx.fillStyle = "#222222";
   ctx.font = "24.0px 'Impact'";
   ctx.fillText("How to Play:", canvas.width / 2 - 100, canvas.height / 2 + 110);
